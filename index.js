1a20037283efbf2249f7b21bb0124f8b93acadad
caomingzhe <caomingzhe@bytedance.com>
2021-11-18 22:50:28 +0800

    project init

---
+++ index.js
@@ -0,0 +1,8 @@
+const introduction = () => {
+  console.log(
+    `hi! This is 404coder's first npm package, he is a handsome man.`
+  );
+  console.log(`404 Not Found Man`);
+};
+
+export default introduction;
