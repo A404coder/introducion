fcc19c7f25f205adcef77fe7bb362bad9ac3444f
A404coder <mzcaogo@gmail.com>
2021-11-19 15:57:01 +0800

    tag v1.0

--- index.js
+++
@@ -1,8 +0,0 @@
-const introduction = () => {
-  console.log(
-    `hi! This is 404coder's first npm package, he is a handsome man.`
-  );
-  console.log(`404 Not Found Man`);
-};
-
-export default introduction;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const introduction = () => {
+    console.log(`hi! This is 404coder's first npm package, he is a handsome man.`);
+    console.log(`404 Not Found Man`);
+};
+
+export { introduction };
